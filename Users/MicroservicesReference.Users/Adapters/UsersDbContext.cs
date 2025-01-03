b9408da7cf4bb0af91b895290783890bb125d604
Jacek Cielecki <jcielecki@sii.pl>
2025-01-03 19:07:18 +0100

    add migrations

--- Users/MicroservicesReference.Users/Adapters/UsersDbContext.cs
+++ Users/MicroservicesReference.Users/Adapters/UsersDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using MicroservicesReference.Users.Core.Model;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace MicroservicesReference.Users.Core.Adapters;
 
@@ -9,5 +10,19 @@ public class UsersDbContext : DbContext
     {
     }
 
-    public DbSet<User> Users { get; set; }
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		base.OnConfiguring(optionsBuilder);
+		//optionsBuilder.UseSqlServer(");
+		//optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("MicroservicesReference_Users_Db"));
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+	}
+
+	public DbSet<User> Users { get; set; }
 }
---
+++ Users/MicroservicesReference.Users/Migrations/20250103175320_AddUsersEntityConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MicroservicesReference.Users.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersEntityConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email" },
+                values: new object[,]
+                {
+                    { new Guid("4f88c524-1738-4827-be54-aeaf1e2f6de4"), "john@example.com" },
+                    { new Guid("b5e645ad-39aa-4912-8b9e-a2a2ebfa72ee"), "mike@example.com" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4f88c524-1738-4827-be54-aeaf1e2f6de4"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("b5e645ad-39aa-4912-8b9e-a2a2ebfa72ee"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
